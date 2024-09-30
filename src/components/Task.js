2c7b4d22141adc50493e0915f4b53b115a622089
V9द <140821136+mrjack002L@users.noreply.github.com>
2024-09-30 08:03:22 +0530

    Update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,40 +1,18 @@
 import React from "react";
-import { Draggable } from "react-beautiful-dnd";
 
-function Task({ tasks, listId }) {
+const Task = React.forwardRef(({ task, provided }, ref) => {
   return (
-    <>
-      {tasks.map((task, index) => {
-        const draggableId = `${listId}-${index}-${task.title}`;
-        
-        // Debugging draggableId
-        console.log("Generated Draggable ID:", draggableId);
-
-        return (
-          <Draggable key={draggableId} draggableId={draggableId} index={index}>
-            {(provided) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.draggableProps}
-                {...provided.dragHandleProps}
-                style={{
-                  padding: "8px",
-                  margin: "0 0 8px 0",
-                  background: "lightgray",
-                  ...provided.draggableProps.style,
-                }}
-              >
-                <h5>{task.title}</h5>
-                <p>{task.description}</p>
-                <p>Due: {task.dueDate}</p>
-                <p>Priority: {task.priority}</p>
-              </div>
-            )}
-          </Draggable>
-        );
-      })}
-    </>
+    <div
+      ref={ref}
+      {...provided.draggableProps}
+      {...provided.dragHandleProps}
+      className="task"
+    >
+      <h5>{task.title}</h5>
+      <p>{task.description}</p>
+      <p>Due Date: {task.dueDate}</p>
+    </div>
   );
-}
+});
 
 export default Task;
