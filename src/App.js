5f25666e22ed0fafd7a9786e0b8d8f3e5a877cc6
V9द <140821136+mrjack002L@users.noreply.github.com>
2024-10-06 16:55:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-//To-Do List
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -9,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import Login from "./components/login";
 import SignUp from "./components/register";
-import Profile from "./components/profile";
+
 import ToDoList from "./components/TodoList";  // Import the ToDoList component
 
 import { auth } from "./components/firebase";
@@ -30,10 +28,10 @@ function App() {
         <div className="auth-wrapper">
           <div className="auth-inner">
             <Routes>
-              <Route path="/" element={user ? <Navigate to="/profile" /> : <Login />} />
+              <Route path="/" element={user ? <Navigate to="/todolist" /> : <Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<SignUp />} />
-              <Route path="/profile" element={<Profile />} />
+              
               <Route path="/todolist" element={<ToDoList user={user} />} /> {/* Add ToDoList Route */}
             </Routes>
             <ToastContainer />
